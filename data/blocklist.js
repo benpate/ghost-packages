54bb8cd902edddb648bd9cf63f040db87c2a1cbd
Ben Pate <ben@pate.org>
2020-05-12 13:47:17 -0600

    Working on Package definition

---
+++ data/blocklist.js
@@ -0,0 +1,15 @@
+[
+	{
+		dns: "*.wholedomain.com",
+		reasons: ["drug use", "propaganda", "violence"],
+		reports: 0,
+		severity: "SEVERE",
+		comments: "This whole domain is shit",
+	},
+	{
+		dns: "joe.commondomain.com",
+		reasons: ["asshole"],
+		severity: "WARNING",
+		comments: "Common Domain is a fine company, but Joe is an asshole."
+	}
+]
